16ed1c2a5e82c376688943fe69883d80eac6462d
Michael Abeyta <mabeyta1990@gmail.com>
2017-12-16 11:29:03 -0700

    ./

--- firebase.js
+++ firebase.js
@@ -1,3 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/4.8.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBw_nMxHxVysJbB4fzRzbiKjqlCwYXaCtU",
+    authDomain: "saturdayactivity-7e6e1.firebaseapp.com",
+    databaseURL: "https://saturdayactivity-7e6e1.firebaseio.com",
+    projectId: "saturdayactivity-7e6e1",
+    storageBucket: "saturdayactivity-7e6e1.appspot.com",
+    messagingSenderId: "360890242926"
+  };
+  firebase.initializeApp(config);
+</script>
+
 $("#submit").on("click", function() {
   $("#employeeTable").last().append("<tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
 });
